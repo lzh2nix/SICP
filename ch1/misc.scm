492c776dec4f69360ad029583020d007ddf2e656
lzh2nix <li1zheng2qian3@gmail.com>
2015-03-09 22:12:34 +0800

    add conf && if && else version of abstract

--- ch1/misc.scm
+++ ch1/misc.scm
@@ -1,3 +1,4 @@
+!/usr/bin/mit-scheme
 ;add a sequence number
 (define add
 (+	(*	3
@@ -8,3 +9,20 @@
 
 ;squre of N
 (define (squre x) (*	x	x))
+
+;sum of squre
+(define	(sum-of-squre x y)
+				(+	(squre x)
+					(squre y)))
+;abstract of x
+(define (abs x)
+	(cond	((> x	0)	x)
+			((= x 0) 0)
+			((< x 0) (- x))))
+
+(define (abs_v1 x)
+	(cond ((< x 0) (- x))
+			(else x)))
+
+(define (abs_v2 x)
+	(if (< x 0) (- x) x))
