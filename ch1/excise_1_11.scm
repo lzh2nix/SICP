8f65c3fb72a3dcca774217de0a65a6a52dad6609
lzh2nix <li1zheng2qian3@gmail.com>
2015-03-13 08:14:16 +0800

    add iterator version of excise 1.11

--- ch1/excise_1_11.scm
+++ ch1/excise_1_11.scm
@@ -6,9 +6,11 @@
 			(* 3 (f_recurise (- n 3))))))
 
 (define (f_iter n)
-	(define (f_iterator a b c n)
-		(if (= n 0) 
-			c
-			(f_iterator b c (+ a (* 2 b) (* 3 c)) (- n 1))))
-	
-	(f_iterator 2 1 0 n))
+	(if (< n 3)
+		n
+	(f_iterator 0 1 2 n)))
+
+(define (f_iterator a b c n)
+	(if (< n 3) 
+		c
+		(f_iterator b c (+ c (* 2 b) (* 3 a)) (- n 1))))
