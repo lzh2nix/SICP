af00736beb264c243fea90cc16f7ca7ac2de76d6
lzh2nix <li1zheng2qian3@gmail.com>
2015-03-19 22:52:15 +0800

    add carmichael check in [a,b]

--- ch1/carmichael-test.scm
+++ ch1/carmichael-test.scm
@@ -1,3 +1,4 @@
+(load "prime")
 (define (expmod base n m)
    (cond ((= n 0) 1)
       ((even? n) (remainder (square (expmod base (/ n 2) m)) m))
@@ -7,7 +8,20 @@
 	(carmichael-test-iter 1 n))
 
 (define (carmichael-test-iter test n)
-	(cond ((= test n) #t)
+	(cond ((and (= test n) (eq? #f (prime? n))) #t)
 		((= (expmod test n n) test) (carmichael-test-iter (+ test 1) n))
 		(else #f)))
+(define (search-for-carmichael m n)
+	(if (< m n)
+		(check-carmichael m n)))
+
+(define (check-carmichael test high)
+	(if (carmichael-test test)
+		(report-carmichael test))
+	(search-for-carmichael (+ test 1) high))
+
+(define (report-carmichael carmichael)
+	(newline)
+	(display " *** ")
+	(display carmichael))
 
