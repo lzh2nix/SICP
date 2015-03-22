11893f7bdc58f79114d079ca26df4d0ff99cbf89
lzh2nix <li1zheng2qian3@gmail.com>
2015-03-22 18:02:56 +0800

    add excise answer code for exise 1.30

---
+++ ch1/excise_1_30.scm
@@ -0,0 +1,20 @@
+;simson-integral(f a b) = h[y(0) +4y(1) + 2y(0) +....+ 2y(n-2) + 4 (n-1) + y(n)]
+;where h = (b-a)/n
+;y(n) = f(a+nh)
+(define(sum term a next b)
+	(define (iter a result)
+		(if (= a b)
+			result
+			(iter (next a) (+ result (term a)))))
+	(iter a 0))
+(define (cube a) (* a a a))
+(define (simson-integral f a b n)
+	(define get-h (/ (- b a) (* n 1.0)))
+	(define (simson-term x)
+		(define y (f (+ a (* x get-h))))
+		(cond ((or (= x 0) (= x n)) y)
+				((even? x) (* 2 y))
+				(else (* 4 y))))
+	(define (next-iter n) (+ n 1))
+	(* (sum simson-term a next-iter n) (/ get-h 3.0)))
+
