0bb035a716bf862ed1d54164c59688da2da62ba7
lzh2nix <li1zheng2qian3@gmail.com>
2015-03-23 20:49:47 +0800

    add half-find method for finding roots of an equation f(x)=0

---
+++ ch1/half-interval-method.scm
@@ -0,0 +1,17 @@
+(define (close-enough? x y) (< (abs (- x y)) 0.001))
+(define (average a b) (/ (+ a b) 2.0))
+;using half-find methods for finding roots of an equation f(x)=0
+(define (search f neg-point pos-point)
+	(let ((mid-point (average neg-point pos-point)))
+		(if (close-enough? neg-point pos-point)
+			mid-point
+			(let ((test-value (f mid-point)))
+				(cond ((positive? test-value) (search f neg-point mid-point))
+						((negative? test-value) (search f mid-point pos-point))
+						(else mid-point))))))
+(define (half-interval-method f a b)
+	(let ((a-value (f a))
+			(b-value (f b)))
+		(cond ((and (positive? a-value) (negative? b-value)) (search f b a))
+				((and (negative? a-value) (positive? b-value)) (search f a b))
+				(else	(error "Values are not of opposite sign " a b)))))
