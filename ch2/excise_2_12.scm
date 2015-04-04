197b49db514d8e7b7bf93cbe4331bc6442302555
lzh2nix <li1zheng2qian3@gmail.com>
2015-04-04 18:05:39 +0800

    add answer code for excise 2.12

---
+++ ch2/excise_2_12.scm
@@ -0,0 +1,23 @@
+(define (low-bound a)
+	(car a))
+
+(define (upper-bound a)
+	(cdr a))
+
+(define (make-interval a b)
+	(cons a b))
+
+(define (make-center-percent center percentage)
+	(make-interval 
+		(- center percentage) 
+		(+ center percentage)))
+
+(define (center i)
+	(/ (+ (low-bound i) (upper-bound i)) 2))
+
+(define (percent i)
+	(/ (* (/ (- (upper-bound i) (low-bound i)) 2) 100) 100))
+
+(define range (make-center-percent 10 (/ 20 100)))
+(center range)
+(percent range)
