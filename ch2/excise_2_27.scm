e7b76e39d40b79a0fe6a3c0c24952660f5dbb40b
lzh2nix <li1zheng2qian3@gmail.com>
2015-04-14 20:23:17 +0800

    fix bug on deep-reverse

--- ch2/excise_2_27.scm
+++ ch2/excise_2_27.scm
@@ -1,14 +1,7 @@
-;
-(define (reverse items)
-	(cond ((not (pair? items)) items)
-			 ((null? (cdr items)) items)
-			(else (append (reverse (cdr items)) (cons (car items) '())))))
-
 (define (deep-reverse items)
 	(cond ((null? items) items)
 			((not (pair? items)) items)
-			((null? (cdr items)) (reverse items))
-			(else (append (deep-reverse (cdr items)) (cons (deep-reverse (car items)) '()))))
+			(else (append (deep-reverse (cdr items)) (cons (deep-reverse (car items)) '())))))
 
 ;test code:
 (deep-reverse (list 1 2 3 4 5))
