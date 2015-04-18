3646060a7d95fa96a26ccadf5ecae6c5c118c961
lzh2nix <li1zheng2qian3@gmail.com>
2015-04-18 10:13:54 +0800

    add answer code for excise 2.36

---
+++ ch2/excise_2_36.scm
@@ -0,0 +1,9 @@
+(define (accumulate-n op init seqs)
+	(if (null? (car seqs))
+		'()
+		(cons (accumulate op init (map (lambda (x) (car x)) seqs))
+				(accumulate-n op init (map (lambda (x) (cdr x)) seqs)))))
+
+
+(map (lambda (x) (car x)) (list (list 1 2 3) (list 4 5 6) (list 7 8 9) (list 10 11 12)))
+(map (lambda (x) (cdr x)) (list (list 1 2 3) (list 4 5 6) (list 7 8 9) (list 10 11 12)))
