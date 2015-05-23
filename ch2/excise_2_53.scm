9892efab4f38615e80bb7d2f704513de5a1c358d
lzh2nix <li1zheng2qian3@gmail.com>
2015-05-23 10:43:32 +0800

    add answer code for excise 2.53

---
+++ ch2/excise_2_53.scm
@@ -0,0 +1,12 @@
+(define (memq item x)
+	(cond ((null? x) #f)
+			((eq? item (car x)) x)
+			(else (memq item (cdr x)))))
+
+(list 'a 'b 'c)
+(list (list 'goerge))
+(cdr '((x1 x2) (y1 y2)))
+(cadr '((x1 x2) (y1 y2)))
+(pair? (car '(a short list)))
+(memq 'red '((red shoes) (blue socks)))
+(memq 'red '(red shoes blue socks))
