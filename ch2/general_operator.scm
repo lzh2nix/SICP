82aa8a9235976a054da9654d91e2d1059e98cd88
lzh2nix <li1zheng2qian3@gmail.com>
2015-06-26 00:33:06 +0800

    add number operator

--- ch2/general_operator.scm
+++ ch2/general_operator.scm
@@ -1,17 +1,17 @@
 (cf "type-tag.scm")
 (load "type-tag")
 
-(cf "install-scheme-number-package.scm")
-(load "install-scheme-number-package")
 (define put 2d-put!)
 (define get 2d-get)
+(cf "install-scheme-number-package.scm")
+(load "install-scheme-number-package")
 (install-scheme-number-package)
 
 (define (apply-generic op . args)
 	(let ((type-tags (map type-tag args)))
-		(let ((proc (get op type-tags)))
+		(let ((proc (get op (car type-tags))))
 			(if proc
-				(apply proc (contents args))
+				(apply proc (map (lambda (x) (cadr x)) args))
 				(error
 					"NO method for these types ----" (list args))))))
 
@@ -22,3 +22,11 @@
 
 (define (make-scheme-number x)
 	((get 'make 'scheme-number) x))
+
+;test
+(define number-1 (make-scheme-number 1))
+(define number-2 (make-scheme-number 2))
+(add number-1 number-2)
+(sub number-1 number-2)
+(mul number-1 number-2)
+(div number-1 number-2)
